838d1ebd25ff0ac5a17d6c5a7c35dd233cefe289
Tratut <64106392+Tratut@users.noreply.github.com>
2020-10-01 18:43:07 +0300

    Add files via upload

--- ft_cmp_str.py
+++ ft_cmp_str.py
@@ -4,7 +4,7 @@ from ft_len import ft_len
 def ft_cmp_str(st1, st2, x):
     res = ''
     for i in range(ft_len(st1)):
-        if i == x:
+        if i == x - 1:
             res += st2
         res += st1[i]
     return res
