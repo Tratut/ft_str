18ce19ed2442b780ac1e9c14ef5ca59d4d3ab56b
Tratut <64106392+Tratut@users.noreply.github.com>
2020-10-01 18:44:47 +0300

    Add files via upload

--- ft_slice_str.py
+++ ft_slice_str.py
@@ -5,6 +5,6 @@ def ft_slice_str(st, start, end):
     if end > ft_len(st):
         end = ft_len(st)
     res = ' '
-    for i in range(start, end):
+    for i in range(start - 1, end):
         res += st[i]
     return res
